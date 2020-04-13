5bea52390be805118028978bb7f17d4fdfb2e5a6
Seewoo <seewoo5@gmail.com>
2020-04-13 22:32:08 +0900

    WIP: SAKT

--- network/SAKT.py
+++ network/SAKT.py
@@ -1,12 +1,22 @@
-# TODO
 import torch
 import torch.nn as nn
 from constant import PAD_INDEX
+from network.transformer.Models import Transformer
 
 
-class SAKT(nn.Module):
-    def __init__(self):
+class SAKT(Transformer):
+    def __init__(self, question_num, input_dim, num_layers, num_head,
+                 inner_dim, key_dim, value_dim, dropout, num_position):
+
+        super().__init__(n_src_vocab=2*question_num+1, n_trg_vocab=question_num+1,
+                         src_pad_idx=PAD_INDEX, trg_pad_idx=PAD_INDEX,
+                         d_word_vec=input_dim, d_model=input_dim, d_inner=inner_dim,
+                         n_layers=num_layers, n_head=num_head, d_k=key_dim, d_v=value_dim,
+                         dropout=dropout, n_position=num_position)
+
+
+    def _transform_interaction_to_question(self, interaction):
         pass
 
-    def forward(self, *input):
+    def forward(self, input, target_id):
         pass
--- network/transformer/Models.py
+++ network/transformer/Models.py
@@ -14,8 +14,7 @@ def get_pad_mask(seq, pad_idx):
 def get_subsequent_mask(seq):
     ''' For masking out the subsequent info. '''
     sz_b, len_s = seq.size()
-    subsequent_mask = (1 - torch.triu(
-        torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
+    subsequent_mask = (1 - torch.triu(torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
     return subsequent_mask
 
 
